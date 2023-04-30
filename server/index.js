39fb7f613999047ee401a819e0bf535f79568056
Abdullah Majeed <abdullahmajeed969@gmail.com>
2023-04-30 15:32:33 +0500

    port adjusment

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import dalleRoutes from './routes/dalle.routes.js';
 import chatRoutes from './routes/chat.routes.js';
 dotenv.config();
-
+const port = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 app.use(express.json({ limig: "50mb" }))
@@ -13,7 +13,8 @@ app.use(express.json({ limig: "50mb" }))
 app.use("/api/v1/dalle", dalleRoutes);
 app.use("/api/v1/chat/completions", chatRoutes);
 app.get('/', (req, res) => {
+  console.log("logs started")
   res.status(200).json({ message: "Hello from DALL.E" })
 })
 
-app.listen(process.env.PORT || 8080, () => console.log('Server has started on port 8080'))
+app.listen(port, () => console.log(`Server has started on port ${port}`))
